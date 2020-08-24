a3a4b9aff64f7af1d3242cd65ea785a17f2ce4b4
Tyler <claypooltyler00@gmail.com>
2020-08-24 03:37:31 -0500

    Changed namespace

--- ExpenseTracker/ExpenseTracker.DataAccess/Repositories/NonBillRepository.cs
+++ ExpenseTracker/ExpenseTracker.DataAccess/Repositories/NonBillRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ExpenseTracker.DataAccess
+namespace ExpenseTracker.DataAccess.Repositories
 {
     public class NonBillRepository : INonBillsRepository, ISave
     {
